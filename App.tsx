8a974087d3dbe5817e4c85059094dac5c4d9f0a7
kaanoner <oner.kaan.k@gmail.com>
2021-05-07 18:13:38 +0300

    defautl theme is added

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import Navigation from './src/navigation';
 import { AuthenticationProvider } from './src/providers/AuthenticationProvider';
-import { Provider as PaperProvider } from 'react-native-paper';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 
 export default function App() {
+  const theme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: 'tomato',
+      accent: 'yellow',
+    },
+  };
   return (
     <AuthenticationProvider>
-      <PaperProvider>
+      <PaperProvider theme={theme}>
         <Navigation />
       </PaperProvider>
     </AuthenticationProvider>
