268918122b1f7c593d74ca965d7364212713f2a6
kaanoner <oner.kaan.k@gmail.com>
2021-05-07 18:12:33 +0300

    react-native-paper config

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navigation from './src/navigation';
 import { AuthenticationProvider } from './src/providers/AuthenticationProvider';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 export default function App() {
   return (
     <AuthenticationProvider>
-
-      <Navigation />
+      <PaperProvider>
+        <Navigation />
+      </PaperProvider>
     </AuthenticationProvider>
   );
 }
